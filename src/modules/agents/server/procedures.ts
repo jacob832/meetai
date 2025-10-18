42d7fa93033e00de3a4af58571bc027f984447d5
jacob832 <jacobhasan832@gmail.com>
2025-10-18 22:36:06 +0300

    12: agents data table

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,30 +1,45 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 import { z } from "zod";
 import { agentInsertSchema } from "../schemas";
 
 export const agentsRouter = createTRPCRouter({
-  getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
-    const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id));
-    return existingAgent;
-  }),
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [existingAgent] = await db
+        .select({
+          meetingCount: sql<number>`5`,
+          ...getTableColumns(agents),
+        })
+        .from(agents)
+        .where(eq(agents.id, input.id));
+      return existingAgent;
+    }),
 
   getMany: protectedProcedure.query(async () => {
-    const data = await db.select().from(agents);
+    const data = await db
+      .select({
+        meetingCount: sql<number>`5`,
+        ...getTableColumns(agents),
+      })
+      .from(agents);
     return data;
   }),
 
   // -> Protected Procedure with input validation Schema
-  create: protectedProcedure.input(agentInsertSchema).mutation(async ({ input, ctx }) => {
-    const [createdAgent] = await db
-      .insert(agents)
-      .values({
-        ...input,
-        userId: ctx.auth.user.id,
-      })
-      .returning();
-    return createdAgent;
-  }),
+  create: protectedProcedure
+    .input(agentInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+      return createdAgent;
+    }),
 });
